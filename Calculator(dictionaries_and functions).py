9fb93a7b3046516504e2531ffd163a7d43a1629e
Dontalashanmukhasai <98035268+Dontalashanmukhasai@users.noreply.github.com>
2023-08-15 12:45:15 +0530

    Calculator(dictionaries_and functions).py

---
+++ Calculator(dictionaries_and functions).py
@@ -0,0 +1,39 @@
+def add(n1,n2):
+    return n1+n2
+
+def subtract(n1,n2):
+    return n1-n2
+
+def multiply(n1,n2):
+    return n1*n2
+
+def divide(n1,n2):
+    return n1/n2
+
+
+operations = {
+    "+" : add,
+    "-" : subtract,
+    "*" : multiply,
+    "/" : divide
+}
+
+def calculator():
+    num1 = float(input("enter your first number?: "))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
+   
+    while should_continue:
+       operation_symbol = input("pick an operation:")
+       num2 = float(input("enter the next number?: "))
+       calculator_function = operations[operation_symbol]
+       answer = calculator_function(num1,num2)
+
+       print(f"{num1} {operation_symbol} {num2} = {answer}")
+       if input(f"Type 'yes' to continue with the {answer} or Type 'no' to restart the calculation:" ) == "yes":
+           num1 = answer
+       else:
+           should_continue = False
+           calculator()
+calculator()
