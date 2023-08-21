24bcf60b7729cfe5bba347f5c9e9b8e252114e16
Dontalashanmukhasai <98035268+Dontalashanmukhasai@users.noreply.github.com>
2023-08-22 01:27:29 +0530

    Coffee_machine_pr.py

--- Coffee_machine_pr.py
+++ Coffee_machine_pr.py
@@ -1,16 +1,13 @@
 from Coffee_machine_data import Menu
 from Coffee_machine_data import resources
-import os
-def system():
-    os.system(cls)
 
-choose_coffee = input("What would you like to have? Type '(espresso/latte/cappuccino)': ")
 def currency():
-    print("Please insert the coins.")
-    print(input("How many ₹1000:"))
-    print(input("How many ₹500:"))
-    print(input("How many ₹200:"))
-    print(input("How many ₹100:"))
+    print("Please insert coins.")
+    total = int(input("how many ₹1000?: "))*1000
+    total += int(input("how many ₹500?: ")) *500
+    total += int(input("how many ₹200?: ")) *200
+    total += int(input("how many ₹100?: ")) *100
+    return total
 
 profit = 0
 def report():
@@ -19,4 +16,37 @@ def report():
     print(f"Coffee : {resources['coffee']}")
     print(f"Amount : {profit}")
 
+def resources_sufficient(ingredients):
+    for item in ingredients:
+        if ingredients[item] >resources[item]:
+            print(f"we are sorry,there is no sufficient {item}")
+            return False
+    return True
 
+def transcation_check(amount_received,drink_cost):
+    if amount_received >= drink_cost:
+        change = round(amount_received-drink_cost,2)
+        print(f"Here is the change ₹{change}. ")
+        return True
+    else:
+        print("Sorry amount is not sufficient! ")
+        return False
+    
+def make_coffe(drink_name,ingredients):
+    for item in ingredients:
+        resources[item] -= ingredients[item]
+    print(f"Here is your {choose_coffee} coffe.")
+
+machine_on = True
+while machine_on:
+    choose_coffee = input("What would you like to have? Type '(espresso/latte/cappuccino)': ")
+    if choose_coffee == "off":
+        machine_on = False
+    elif choose_coffee == "report":
+        report()
+    else:
+        coffee = Menu[choose_coffee]
+        if resources_sufficient(coffee["ingredients"]):
+            payment = currency()
+            if transcation_check(payment,coffee["cost"]):
+                make_coffe(choose_coffee,coffee["ingredients"])
